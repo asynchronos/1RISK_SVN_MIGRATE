94e4e6e01589f96cafee0a01b5491e58a791e646
big <big@071a22bb-c5d6-6042-9f9a-ff8927fd5cf9>
2011-04-28 12:54:05 +0000

    form bind complete
    must add casecade dropdown 
    checkbox filter

--- LGDCollectionData/branches/Big20110428/Aspx/CASHFLOW.aspx.designer.cs
+++ LGDCollectionData/branches/Big20110428/Aspx/CASHFLOW.aspx.designer.cs
@@ -47,5 +47,14 @@ namespace LGDCollectionData.Aspx {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.SqlDataSource Currency_DataSource;
+        
+        /// <summary>
+        /// PreviousLimitNo_DataSource control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.SqlDataSource PreviousLimitNo_DataSource;
     }
 }
