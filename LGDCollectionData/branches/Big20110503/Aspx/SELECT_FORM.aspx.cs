37a4f40ea27394cc846962904b2085e031bccff5
big <big@071a22bb-c5d6-6042-9f9a-ff8927fd5cf9>
2011-05-03 10:29:00 +0000

    commit before add new LGDEntities

--- LGDCollectionData/branches/Big20110503/Aspx/SELECT_FORM.aspx.cs
+++ LGDCollectionData/branches/Big20110503/Aspx/SELECT_FORM.aspx.cs
@@ -1,21 +1,42 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
+using LGDCollectionData.Entities;
+using log4net;
 
 namespace LGDCollectionData.Aspx
 {
-    public partial class SELECT_FORM : MyAspxPage 
+    public partial class SELECT_FORM : MyAspxPage
     {
+        private static readonly ILog log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        private static readonly bool isDebugEnabled = log.IsDebugEnabled;
+
+        [System.Web.Services.WebMethodAttribute(), System.Web.Script.Services.ScriptMethodAttribute()]
+        public static string[] GetCompletionList(string prefixText, int count, string contextKey)
+        {
+            using (LGDEntities en = new LGDEntities())
+            {
+                //var queryUser = (from user in en.lis
+                //                 where user.EMP_ID == username
+                //                 && user.EXPIRE_DATE >= DateTime.Now
+                //                 && user.DEL_FLAG == false
+                //                 select user);
+
+                //if (!(queryUser.FirstOrDefault() == null))
+                //{
+                //    if (queryUser.FirstOrDefault().PASSWD.Equals(password))
+                //    {
+                //        result = true;
+                //    }
+                //}
+            }
+            return default(string[]);
+        }
+
         protected void Page_Load(object sender, EventArgs e)
         {
-
         }
 
         protected void ButtonOpenForm_Click(object sender, EventArgs e)
-        {  
+        {
             if (TextBoxCIF.Text != null)
             {
                 Response.Redirect(DropDownListFormName.SelectedValue + "?CIF=" + TextBoxCIF.Text);
--- LGDCollectionData/branches/Big20110503/Aspx/SELECT_FORM.aspx.designer.cs
+++ LGDCollectionData/branches/Big20110503/Aspx/SELECT_FORM.aspx.designer.cs
@@ -58,6 +58,15 @@ namespace LGDCollectionData.Aspx {
         protected global::System.Web.UI.WebControls.TextBox TextBoxCIF;
         
         /// <summary>
+        /// TextBoxCIF_AutoCompleteExtender control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::AjaxControlToolkit.AutoCompleteExtender TextBoxCIF_AutoCompleteExtender;
+        
+        /// <summary>
         /// Label1 control.
         /// </summary>
         /// <remarks>
