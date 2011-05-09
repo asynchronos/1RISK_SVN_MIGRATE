3fc70e7af982f6407188f95a56806cb4aa284958
big <big@071a22bb-c5d6-6042-9f9a-ff8927fd5cf9>
2011-05-09 10:28:17 +0000

    merger big20110504

--- LGDCollectionData/trunk/Aspx/OBLIGOR_INFORMATION.aspx.cs
+++ LGDCollectionData/trunk/Aspx/OBLIGOR_INFORMATION.aspx.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Web.UI.WebControls;
 using log4net;
 
 namespace LGDCollectionData.Aspx
@@ -15,43 +14,27 @@ namespace LGDCollectionData.Aspx
 
         protected void OBLIGOR_INFORMATION_DetailsView_PreRender(object sender, EventArgs e)
         {
-            DetailsView dv = (DetailsView)sender;
+            //DetailsView dv = (DetailsView)sender;
 
-            if (dv.CurrentMode == DetailsViewMode.Edit)
-            {
-                if (dv.FindControl("Data_Inputter_TextBox") != null)
-                {
-                    if (User.Identity.Name.Equals(String.Empty))
-                    {
-                        //throw new Exception("Session Time out:Please login again.");
-                        ((TextBox)dv.FindControl("Data_Inputter_TextBox")).Text = User.Identity.Name;
-                    }
-                    else
-                    {
-                        ((TextBox)dv.FindControl("Data_Inputter_TextBox")).Text = User.Identity.Name;
-                    }
-                }
+            //if (dv.CurrentMode == DetailsViewMode.Edit)
+            //{
+            //    if (dv.FindControl("Data_Inputter_TextBox") != null)
+            //    {
+            //        if (User.Identity.Name.Equals(String.Empty))
+            //        {
+            //            throw new Exception("Session Time out:Please login again.");
+            //        }
+            //        else
+            //        {
+            //            ((TextBox)dv.FindControl("Data_Inputter_TextBox")).Text = User.Identity.Name;
+            //        }
+            //    }
 
-                if (dv.FindControl("Date_of_Input_TextBox") != null)
-                {
-                    ((TextBox)dv.FindControl("Date_of_Input_TextBox")).Text = "Today";
-                }
-            }
-        }
-
-        protected override void DetailsView_ItemUpdating(object sender, System.Web.UI.WebControls.DetailsViewUpdateEventArgs e)
-        {
-            if (!User.Identity.Name.Equals(String.Empty))
-            {
-                e.NewValues["Data_Inputter"] = User.Identity.Name;
-                e.NewValues["Date_of_Input"] = DateTime.Now;
-            }
-            else
-            {
-                //throw new Exception("Session Time out:Please login again.");
-                e.NewValues["Data_Inputter"] = User.Identity.Name;
-                e.NewValues["Date_of_Input"] = DateTime.Now;
-            }
+            //    if (dv.FindControl("Date_of_Input_TextBox") != null)
+            //    {
+            //        ((TextBox)dv.FindControl("Date_of_Input_TextBox")).Text = DateTime.Now.ToString("d/M/yyyy");
+            //    }
+            //}
         }
     }
 }
