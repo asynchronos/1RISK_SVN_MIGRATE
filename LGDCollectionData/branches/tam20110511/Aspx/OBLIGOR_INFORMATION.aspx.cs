6c7fcd84b81ed66ebbe20d011a111abc0b75db09
pichaya <pichaya@071a22bb-c5d6-6042-9f9a-ff8927fd5cf9>
2011-05-11 03:07:33 +0000

    141

---
+++ LGDCollectionData/branches/tam20110511/Aspx/OBLIGOR_INFORMATION.aspx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using log4net;
+
+namespace LGDCollectionData.Aspx
+{
+    public partial class OBLIGOR_INFORMATION : MyAspxPage
+    {
+        private static readonly ILog log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        private static readonly bool isDebugEnabled = log.IsDebugEnabled;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+
+        protected void OBLIGOR_INFORMATION_DetailsView_PreRender(object sender, EventArgs e)
+        {
+            //DetailsView dv = (DetailsView)sender;
+
+            //if (dv.CurrentMode == DetailsViewMode.Edit)
+            //{
+            //    if (dv.FindControl("Data_Inputter_TextBox") != null)
+            //    {
+            //        if (User.Identity.Name.Equals(String.Empty))
+            //        {
+            //            throw new Exception("Session Time out:Please login again.");
+            //        }
+            //        else
+            //        {
+            //            ((TextBox)dv.FindControl("Data_Inputter_TextBox")).Text = User.Identity.Name;
+            //        }
+            //    }
+
+            //    if (dv.FindControl("Date_of_Input_TextBox") != null)
+            //    {
+            //        ((TextBox)dv.FindControl("Date_of_Input_TextBox")).Text = DateTime.Now.ToString("d/M/yyyy");
+            //    }
+            //}
+        }
+    }
+}
