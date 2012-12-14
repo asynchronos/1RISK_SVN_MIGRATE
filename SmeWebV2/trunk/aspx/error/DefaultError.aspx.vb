8b27d5340a956d1f58d0485deaa74429abe36174
big <big@071a22bb-c5d6-6042-9f9a-ff8927fd5cf9>
2012-12-14 04:15:27 +0000

    edit login, log, error

---
+++ SmeWebV2/trunk/aspx/error/DefaultError.aspx.vb
@@ -0,0 +1,21 @@
+﻿
+Partial Class aspx_error_DefaultError
+    Inherits aspx.MyPageClass
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        Dim page As String = String.Empty
+        Dim msg As String = String.Empty
+
+        If Not IsNothing(Application("PageError")) Then
+            Page = Application("PageError").ToString()
+        End If
+
+        If Not IsNothing(Application("LastError")) Then
+            msg = (DirectCast(Application("LastError"), Exception)).Message
+        End If
+
+        Error_Label.Text = "Error : Please contact administrator.<br />" _
+                & "Page : " + page + "<br />" _
+                & "Message : " + msg + "<br />"
+    End Sub
+End Class
