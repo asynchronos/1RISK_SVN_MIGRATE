47029e851b62a24c7e686f7f659df1f70285d07c
big <big@071a22bb-c5d6-6042-9f9a-ff8927fd5cf9>
2012-12-18 03:24:27 +0000

    Add Method to clean up old log file

---
+++ SME.DebtSummary/trunk/Util/Log4net/StringExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics;
+
+namespace Util.Log4net
+{
+    /// <summary>
+    /// Extension helper methods for strings
+    /// </summary>
+    [DebuggerStepThrough, DebuggerNonUserCode]
+    public static class StringExtensions
+    {
+        /// <summary>
+        /// Formats a string using the <paramref name="format"/> and <paramref name="args"/>.
+        /// </summary>
+        /// <param name="format">The format.</param>
+        /// <param name="args">The args.</param>
+        /// <returns>A string with the format placeholders replaced by the args.</returns>
+        public static string Sub(this string format, params object[] args)
+        {
+            return string.Format(format, args);
+        }
+    }
+}
